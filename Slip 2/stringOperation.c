f3d74923dd307803c22928417071e81779878539
Piyush Dive <piyushdive835@gmail.com.com>
2022-06-07 11:32:48 +0530

    Slip 2 updated

--- Slip 2/stringOperation.c
+++ Slip 2/stringOperation.c
@@ -12,37 +12,50 @@
 
 void main(){
     char str1[100], str2[100];
+    int ch, a;
     printf("Enter string 1: ");
     scanf("%s", str1);
     printf("Enter string 2: ");
     scanf("%s", str2);
     printf("\n");
 
-    // Convert string to uppercase
-    
-    strupr(str1);
-    strupr(str2);
-    printf("%s %s\n", str1, str2);
+    printf("Enter choice: \n");
+    printf("1. Uppercase\n2. Copy\n3. Compare\n4. Concatenate\n");
+    scanf("%d", &ch);
 
-    // Comapre two strings
+    switch(ch){
+        case 1:
+            // Convert string to uppercase
+            strupr(str1);
+            strupr(str2);
+            printf("%s %s\n", str1, str2);
+            break;
+        
+        case 2:
+            // Copy one string to another
+            strcpy(str1, str2);
+            puts(str1);
+            break;
 
-    int a = strcmp(str1, str2);
-    if (a > 0)
-        printf("%s is bigger than %s.\n", str1, str2);
+        case 3:
+            // Comapre two strings
+            a = strcmp(str1, str2);
+            if (a > 0)
+                printf("%s is bigger than %s.\n", str1, str2);
 
-    else if (a < 0)
-        printf("%s is bigger than %s.\n", str2, str1);
+            else if (a < 0)
+                printf("%s is bigger than %s.\n", str2, str1);
 
-    else
-        printf("Both strings are equal.\n");
-    
-    // Copy one string to another
+            else
+                printf("Both strings are equal.\n");
+            break;
 
-    strcpy(str1, str2);
-    puts(str1);
-
-    // Conatenate two strings
-
-    strcat(str1, str2);
-    puts(str1);
+        case 4:
+            // Conatenate two strings
+            strcat(str1, str2);
+            puts(str1);
+            break;
+        
+        default:printf("Invalid choice\n");
+    }
 }
